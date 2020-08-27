2f45ebce427806d8357dba1559d9706c0b789d56
Mubbashir Ali <mubbashirali35@gmail.com>
2020-08-28 03:35:24 +0500

    Header Completed 0.0.0

--- client/src/Components/HeaderComponent/HeaderComponent.js
+++ client/src/Components/HeaderComponent/HeaderComponent.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 import './HeaderComponent.css';
 
@@ -9,6 +12,20 @@ export default function HeaderComponent() {
             <Navbar.Brand>
                 <h1 className='brand' >Foody</h1>
             </Navbar.Brand>
+            <Nav className='ml-auto'>
+                <Nav.Item className='mr-2'>
+                    <Nav.Link className='link' style={{ color: '#000', paddingLeft: '1rem',
+                        paddingRight: '1rem' }}>
+                        <FontAwesomeIcon icon={faUser} className='mr-2'/>
+                        LOGIN
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link style={{ color: '#000' }}>
+                        <FontAwesomeIcon icon={faShoppingCart} />
+                    </Nav.Link>
+                </Nav.Item>
+            </Nav>
         </Navbar>
     )
 }
