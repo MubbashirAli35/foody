fe16900916ddea5d69153bacc781c3daff5375d3
Mubbashir Ali <mubbashirali35@gmail.com>
2020-08-28 04:38:54 +0500

    Heading for registering your restaurant added

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
 
 import HeaderComponent from '../../Components/HeaderComponent/HeaderComponent';
 import './Home.css';
 
 export default function Home() {
     return(
-        <Container className='px-0' expand='md' fluid>
+        <Container style={{ maxWidth: '100vw' }} className='px-0' expand='md' fluid>
             <HeaderComponent />
 
             <Container className='bgContainer' fluid>
             </Container>
+
+            <Col className='offset-md-1 my-5' sm={12} md={11}>
+                <h3>
+                    We'll deliver it, just prepare the food
+                </h3>
+            </Col>
         </Container>
     );
 }
